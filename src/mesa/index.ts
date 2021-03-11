2286c19c24c2bc22bcff55eec2a3764005b220ff
Adam Azad <adam@adamazad.com>
2021-03-11 12:33:37 +0300

    Add mesa lib

---
+++ src/mesa/index.ts
@@ -0,0 +1,73 @@
+// Externals
+import { Provider } from '@ethersproject/abstract-provider'
+import { BigNumberish, Signer } from 'ethers'
+
+// EasyAuction Contracts
+import { EasyAuction__factory, EasyAuction } from '../contracts'
+
+interface CreateAuctionProps {
+  auctionId: BigNumberish
+  auctioningToken: string
+  biddingToken: string
+  orderCancellationEndDate: BigNumberish
+  auctionEndDate: BigNumberish
+  auctionedSellAmount: BigNumberish
+  minBuyAmount: BigNumberish
+  minimumBiddingAmountPerOrder: BigNumberish
+  minFundingThreshold: BigNumberish
+}
+
+export function getEasyAuctionContract(contractAddress: string, signerOrProvider: Signer | Provider) {
+  return EasyAuction__factory.connect(contractAddress, signerOrProvider)
+}
+
+/**
+ *
+ * @param easyAuctionContract the EasyAuction contract instance (with signer)
+ * @param option
+ * @param overrides
+ * @returns
+ */
+export async function createAuction(
+  easyAuctionContract: EasyAuction,
+  {
+    auctionId,
+    auctioningToken,
+    biddingToken,
+    orderCancellationEndDate,
+    auctionEndDate,
+    auctionedSellAmount,
+    minBuyAmount,
+    minimumBiddingAmountPerOrder,
+    minFundingThreshold,
+  }: CreateAuctionProps
+) {
+  const createAuctionTx = await easyAuctionContract.emitNewAuction(
+    auctionId,
+    auctioningToken,
+    biddingToken,
+    orderCancellationEndDate,
+    auctionEndDate,
+    auctionedSellAmount,
+    minBuyAmount,
+    minimumBiddingAmountPerOrder,
+    minFundingThreshold
+  )
+  return createAuctionTx
+}
+
+interface CreateAuctionBidProps {
+  auctionId: BigNumberish
+  userId: BigNumberish
+  buyAmount: BigNumberish
+  sellAmount: BigNumberish
+}
+
+export async function createAuctionBid(
+  easyAuctionContract: EasyAuction,
+  { auctionId, userId, buyAmount, sellAmount }: CreateAuctionBidProps
+) {
+  const createAuctionBidTx = await easyAuctionContract.emitNewSellOrder(auctionId, userId, buyAmount, sellAmount)
+
+  return createAuctionBidTx
+}
